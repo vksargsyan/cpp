cea16b29139203ad7216ae205b916eeb50014f8f
Vahagn <v.k.sargsyan@gmail.com>
2019-03-17 14:18:37 +0400

    factorial problem

--- factorial.cpp
+++ factorial.cpp
@@ -2,6 +2,7 @@
 
 /**
  * Factorial program
+ * http://coliru.stacked-crooked.com/a/7a9b5d7d9f6ffe2a
  */
  
 int factorial(int num) {
@@ -16,4 +17,5 @@ int main()
     int f = factorial(0);
     
     std::cout << f << std::endl;
+	return 0;
 }
